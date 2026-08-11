05edfc287b7faca90e845adea59045fd84c063c8
hBISPO98 <b.hiandra@aluno.ifsp.edu.br>
2026-08-11 01:58:47 +0000

    Aplicação Web com inserção de Templates

--- hello.py
+++ hello.py
@@ -1,44 +1,52 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, redirect, request, make_response, abort
+from datetime import datetime
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-# rota 1: hello world
 @app.route('/')
 def index():
-    return f'<h1>Hello World!</h1><h2>Disciplina PTBDSWS</h2>'
+    # chama o arquivo index.html que está dentro da pasta templates/
+    return render_template('index.html', current_time=datetime.utcnow())
 
-# rota 2: hello name
 @app.route('/user/<name>')
 def user(name):
-    return f'<h1>Hello, {name}!</h1>'
+    # chama o arquivo user.html
+    return render_template('user.html', name=name)
 
-# rota 3: contexto de requisição
+# rota para Identificação
+@app.route('/identificacao')
+def identificacao():
+    return render_template(
+        'user.html',
+        name="Hiandra",
+        prontuario="PT3038513",
+        meutitulo="IFSP"
+    )
+
+# rota para Contexto da Requisição
 @app.route('/contextorequisicao')
 def contexto_requisicao():
     user_agent = request.headers.get('User-Agent')
-    return f'<p>Your browser is {user_agent}</p>'
+    remote_ip = request.remote_addr
+    host = request.host
+    return render_template(
+        'contexto.html',
+        name="Hiandra",
+        user_agent=user_agent,
+        remote_ip=remote_ip,
+        host=host
+    )
 
-# rota 4: status diferente - 400
-@app.route('/codigostatusdiferente')
-def codigo_status_diferente():
-    return '<p>Bad request</p>', 400
+@app.errorhandler(404)
+def page_not_found(e):
+    # chama o arquivo 404.html
+    return render_template('404.html'), 404
 
-# rota 5: objeto resposta
-@app.route('/objetoresposta')
-def objeto_resposta():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('language','pt-BR')
-    return response
-
-# rota 6: redirecionamento para o site IFSP
-@app.route('/ifsp')
-def redirecionar_ifsp():
-    return redirect('https://ptb.ifsp.edu.br/')
-
-# rota 7: cancelamento
-@app.route('/cancelar')
-def cancelar_requisicao():
-    abort(404)
+@app.errorhandler(500)
+def internal_server_error(e):
+    # chama o arquivo 500.html
+    return render_template('500.html'), 500
